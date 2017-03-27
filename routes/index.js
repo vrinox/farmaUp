1d5fb02434ee54dc74787efafd62f72023f0f568
vleon <vleon@socaportuguesa.com>
2017-03-27 14:39:58 -0400

    primera

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var models = require('../server/models/index');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/users', function(req, res) {
+  models.User.create({
+    email: req.body.email
+  }).then(function(user) {
+    res.json(user);
+  });
+});
+//------------------------ TODOS ---------------------
+//obteber todos
+router.get('/todos', function(req, res) {
+  models.Todo.findAll({}).then(function(todos) {
+    res.json(todos);
+  });
+});
+//guardar registro
+router.post('/todos', function(req, res) {
+  models.Todo.create({
+    title: req.body.title,
+    UserId: req.body.user_id
+  }).then(function(todo) {
+    res.json(todo);
+  });
+});
+//buscar uno solo
+router.get('/todo/:id', function(req, res) {
+  models.Todo.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(todo) {
+    res.json(todo);
+  });
+});
+//modificar
+router.put('/todo/:id', function(req, res) {
+  models.Todo.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(todo) {
+    if(todo){
+      todo.updateAttributes({
+        title: req.body.title,
+        complete: req.body.complete
+      }).then(function(todo) {
+        res.send(todo);
+      });
+    }
+  });
+});
+// delete a single todo
+router.delete('/todo/:id', function(req, res) {
+  models.Todo.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(todo) {
+    res.json(todo);
+  });
+});
+module.exports = router;
---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    email: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        User.hasMany(models.Todo);
+      }
+    }
+  });
+  return User;
+};
